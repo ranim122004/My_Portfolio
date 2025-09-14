2aeceb5a9a3e01e6a06273b1f6b8959f847c6ce5
Ranim <ranim122004@gmail.com>
2025-09-14 16:22:13 +0300

    Initial commit

---
+++ js/portfolios.js
@@ -0,0 +1,338 @@
+var portfolio_data = [
+	{
+		id: 'projects-rhu-career-platform',
+		images: [
+			{
+				src: 'images/rhucareerplatform/landingpage.jpg',
+				thumb: 'images/rhucareerplatform/landingpage.jpg',
+				subHtml: '<h4>Landing Page</h4><p>University Career Platform Project</p>'
+			},
+			{
+				src: 'images/rhucareerplatform/quizes.jpg',
+				thumb: 'images/rhucareerplatform/quizes.jpg',
+				subHtml: '<h4>Quizes Section</h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/studenthomepage.jpg',
+				thumb: 'images/rhucareerplatform/studenthomepage.jpg',
+				subHtml: '<h4>Student HomePage </h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/studentdashboard.jpg',
+				thumb: 'images/rhucareerplatform/studentdashboard.jpg',
+				subHtml: '<h4>Student Dashboard </h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/alumnihomepage.jpg',
+				thumb: 'images/rhucareerplatform/alumnihomepage.jpg',
+				subHtml: '<h4>Alumni HomePage </h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/alumnidashboard.jpg',
+				thumb: 'images/rhucareerplatform/alumnidashboard.jpg',
+				subHtml: '<h4>Alumni Dashboard </h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/jobsearch.jpg',
+				thumb: 'images/rhucareerplatform/jobsearch.jpg',
+				subHtml: '<h4>Job Landing Page </h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/jobopportunity.jpg',
+				thumb: 'images/rhucareerplatform/jobopportunity.jpg',
+				subHtml: '<h4>Job Opportunities </h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/jobsearching.jpg',
+				thumb: 'images/rhucareerplatform/jobsearching.jpg',
+				subHtml: '<h4>Smart Job Searching </h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/applyjob.jpg',
+				thumb: 'images/rhucareerplatform/applyjob.jpg',
+				subHtml: '<h4>Applying to a job </h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/availablementor.jpg',
+				thumb: 'images/rhucareerplatform/availablementor.jpg',
+				subHtml: '<h4>Available Mentors</h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/matchingskills.jpg',
+				thumb: 'images/rhucareerplatform/matchingskills.jpg',
+				subHtml: '<h4>Skills Matching Careers </h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/Analysiscv.jpg',
+				thumb: 'images/rhucareerplatform/Analysiscv.jpg',
+				subHtml: '<h4>Resume Analysis </h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/profesinalhomepage.jpg',
+				thumb: 'images/rhucareerplatform/profesinalhomepage.jpg',
+				subHtml: '<h4>Skills Matching Careers </h4><p>University Career Platform Project</p>',
+			},
+			{
+				src: 'images/rhucareerplatform/admindashboard.jpg',
+				thumb: 'images/rhucareerplatform/admindashboard.jpg',
+				subHtml: '<h4>Skills Matching Careers </h4><p>University Career Platform Project</p>',
+			},
+		],
+	},
+	{
+		id: 'influhub',
+		images: [
+			{
+				src: './images/influhub/signin.png',
+				thumb: './images/influhub/signin.png',
+				subHtml: '<h4>SignIn Page</h4><p>Full-stack Mobile App</p>',
+			},
+			{
+				src: './images/influhub/signup.png',
+				thumb: './images/influhub/signup.png',
+				subHtml: '<h4>SignUp Page</h4><p>Full-stack Mobile App</p>',
+			},
+			{
+				src: './images/influhub/otp.png',
+				thumb: './images/influhub/otp.png',
+				subHtml: '<h4>OTP Verification Page</h4><p>Full-stack Mobile App</p>',
+			},
+			{
+				src: './images/influhub/forgot.png',
+				thumb: './images/influhub/forgot.png',
+				subHtml: '<h4>Forgot Password Page</h4><p>Full-stack Mobile App</p>',
+			},
+				{
+				src: './images/influhub/otpinput.png',
+				thumb: './images/influhub/otpinput.png',
+				subHtml: '<h4>OTP Verification Page</h4><p>Full-stack Mobile App</p>',
+			},
+			{
+				src: './images/influhub/reset.png',
+				thumb: './images/influhub/reset.png',
+				subHtml: '<h4>Reset Password Page</h4><p>Full-stack Mobile App</p>',
+			},
+			{
+				src: './images/influhub/profileonboarding.png',
+				thumb: './images/influhub/profileonboarding.png',
+				subHtml: '<h4>Profile OnBoarding Page</h4><p>Full-stack Mobile App</p>',
+			},
+			{
+				src: './images/influhub/retailerdashboard.png',
+				thumb: './images/influhub/retailerdashboard.png',
+				subHtml: '<h4>Retailer Dashboard Page</h4><p>Full-stack Mobile App</p>',
+			},
+			{
+				src: './images/influhub/retailerprofile.png',
+				thumb: './images/influhub/retailerprofile.png',
+				subHtml: '<h4>Retailer Profile Page</h4><p>Full-stack Mobile App</p>',
+			},
+			{
+				src: './images/influhub/changepass.png',
+				thumb: './images/influhub/changepass.png',
+				subHtml: '<h4>Change Password Page</h4><p>Full-stack Mobile App</p>',
+			},
+			{
+				src: './images/influhub/address.png',
+				thumb: './images/influhub/address.png',
+				subHtml: '<h4>Address Page</h4><p>Full-stack Mobile App</p>',
+			},
+			
+		],
+	},
+	{
+		id: 'uniflow',
+		images: [
+			{
+				src: './images/uniflow/signin.png',
+				thumb: './images/uniflow/signin.png',
+				subHtml: '<h4>SignIn Page</h4><p> Project & Task Manager</p>',
+			},
+			{
+				src: './images/uniflow/signup.png',
+				thumb: './images/uniflow/signup.png',
+				subHtml: '<h4>SignUp Page</h4><p> Project & Task Manager</p>',
+			},{
+				src: './images/uniflow/dashboard.png',
+				thumb: './images/uniflow/dashboard.png',
+				subHtml: '<h4>Dashboard Page</h4><p> Project & Task Manager</p>',
+			},{
+				src: './images/uniflow/projectpage.png',
+				thumb: './images/uniflow/projectpage.png',
+				subHtml: '<h4>Projects Page</h4><p> Project & Task Manager</p>',
+			}
+			
+		,
+		{
+				src: './images/uniflow/completedpage.png',
+				thumb: './images/uniflow/completedpage.png',
+				subHtml: '<h4>Completed Projects Page</h4><p> Project & Task Manager</p>',
+			},{
+				src: './images/uniflow/projectdetail.png',
+				thumb: './images/uniflow/projectdetail.png',
+				subHtml: '<h4>Project Details Page</h4><p> Project & Task Manager</p>',
+			},
+			{
+				src: './images/uniflow/calender.png',
+				thumb: './images/uniflow/calender.png',
+				subHtml: '<h4>Calander Page</h4><p> Project & Task Manager</p>',
+			},
+			
+		],
+	},
+
+	{
+		id: 'bookstore',
+		images: [
+			{
+				src: './images/bookstore/about.png',
+				thumb: './images/bookstore/about.png',
+				subHtml: '<h4>About Page</h4><p>BookShop</p>',
+			},{
+				src: './images/bookstore/shop.png',
+				thumb: './images/bookstore/shop.png',
+				subHtml: '<h4>Shop Page</h4><p>BookShop</p>',
+			},{
+				src: './images/bookstore/search.png',
+				thumb: './images/bookstore/search.png',
+				subHtml: '<h4>Searching for a Product</h4><p>BookShop</p>',
+			},
+			{
+				src: './images/bookstore/order.png',
+				thumb: './images/bookstore/order.png',
+				subHtml: '<h4>Orders </h4><p>BookShop</p>',
+			},{
+				src: './images/bookstore/contact.png',
+				thumb: './images/bookstore/contact.png',
+				subHtml: '<h4> Contact Page</h4><p>BookShop</p>',
+			},{
+				src: './images/bookstore/admin.png',
+				thumb: './images/bookstore/admin.png',
+				subHtml: '<h4>Admin Pannel</h4><p>BookShop</p>',
+			},
+			{
+				src: './images/bookstore/addproduct.png',
+				thumb: './images/bookstore/addproduct.png',
+				subHtml: '<h4>Add Products</h4><p>BookShop</p>',
+			},{
+				src: './images/bookstore/adminupdate.png',
+				thumb: './images/bookstore/adminupdate.png',
+				subHtml: '<h4>Updating Products</h4><p>BookShop</p>',
+			},
+		],
+	},
+	{
+		id: 'marketing-agency',
+		images: [
+			{
+				src: './images/agency/register.png',
+				thumb: './images/agency/register.png',
+				subHtml: '<h4>Registration  Page</h4><p>Frontend Website</p>',
+			},
+			{
+				src: './images/agency/about.png',
+				thumb: './images/agency/about.png',
+				subHtml: '<h4>About Us Page</h4><p>Frontend Website</p>',
+			},
+			{
+				src: './images/agency/team.png',
+				thumb: './images/agency/team.png',
+				subHtml: '<h4>Our Team Page</h4><p>Frontend Website</p>',
+			},{
+				src: './images/agency/services.png',
+				thumb: './images/agency/services.png',
+				subHtml: '<h4>Services Page</h4><p>Frontend Website</p>',
+			},{
+				src: './images/agency/contact.png',
+				thumb: './images/agency/contact.png',
+				subHtml: '<h4>Contact Us  Page</h4><p>Frontend Website</p>',
+			},
+		],
+	},
+	{
+		id: 'zouyout',
+		images: [
+			{
+				src: './images/zoyout/signin.png',
+				thumb: './images/zoyout/signin.png',
+				subHtml:
+					'<h4>SignIn </h4><p>Figma → Web · Responsive · Accessible</p>',
+			},{
+				src: './images/zoyout/signup.png',
+				thumb: './images/zoyout/signup.png',
+				subHtml:
+					'<h4>Signup </h4><p>Figma → Web · Responsive · Accessible</p>',
+			},
+			{
+				src: './images/zoyout/products.png',
+				thumb: './images/zoyout/products.png',
+				subHtml:
+					'<h4>Best Seller </h4><p>Figma → Web · Responsive · Accessible</p>',
+			},{
+				src: './images/zoyout/best.png',
+				thumb: './images/zoyout/best.png',
+				subHtml:
+					'<h4>Why Choose These Products</h4><p>Figma → Web · Responsive · Accessible</p>',
+			},{
+				src: './images/zoyout/choose.png',
+				thumb: './images/zoyout/choose.png',
+				subHtml:
+					'<h4>Speciality </h4><p>Figma → Web · Responsive · Accessible</p>',
+			},
+			
+			{
+				src: './images/zoyout/about.png',
+				thumb: './images/zoyout/about.png',
+				subHtml:
+					'<h4>About Us </h4><p>Figma → Web · Responsive · Accessible</p>',
+			},{
+				src: './images/zoyout/products.png',
+				thumb: './images/zoyout/products.png',
+				subHtml:
+					'<h4>Shop Page </h4><p>Figma → Web · Responsive · Accessible</p>',
+			},
+			{
+				src: './images/zoyout/skin.png',
+				thumb: './images/zoyout/skin.png',
+				subHtml:
+					'<h4>Scan Your Skin</h4><p>Figma → Web · Responsive · Accessible</p>',
+			},
+			{
+				src: './images/zoyout/cart.png',
+				thumb: './images/zoyout/cart.png',
+				subHtml:
+					'<h4>Cart Page </h4><p>Figma → Web · Responsive · Accessible</p>',
+			},
+			{
+				src: './images/zoyout/informaton.png',
+				thumb: './images/zoyout/informaton.png',
+				subHtml:
+					'<h4>Card Information </h4><p>Figma → Web · Responsive · Accessible</p>',
+			},
+		],
+	},
+	{
+		id: 'doctor',
+		images: [
+			{
+				src: './images/doctor/dashboard.jpg',
+				thumb: './images/doctor/dashboard.jpg',
+				subHtml: '<h4>Dashboard </h4><p>Android Application</p>',
+			},
+			{
+				src: './images/doctor/doctordetails.jpg',
+				thumb: './images/doctor/doctordetails.jpg',
+				subHtml: '<h4>Doctor Details </h4><p>Android Application</p>',
+			},{
+				src: './images/doctor/makeapp.jpg',
+				thumb: './images/doctor/makeapp.jpg',
+				subHtml: '<h4>Make An Appointment </h4><p>Android Application</p>',
+			},{
+				src: './images/doctor/bookedapp.jpg',
+				thumb: './images/doctor/bookedapp.jpg',
+				subHtml: '<h4>Book An Appointment </h4><p>Android Application</p>',
+			},
+		],
+	},
+	
+];
